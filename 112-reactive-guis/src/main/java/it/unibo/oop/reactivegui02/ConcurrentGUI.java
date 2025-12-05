35d227eebb81c4226e7f86e94980beaec1c22e91
Lorenzo Belletti <lorenzo.belletti5@unibo.studio.it>
2025-12-05 16:17:14 +0100

    Ultimate reactiveGui2

--- 112-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
+++ 112-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
@@ -4,10 +4,15 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import it.unibo.oop.JFrameUtil;
 
 import java.io.Serial;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * Second example of reactive GUI.
@@ -16,6 +21,7 @@ public final class ConcurrentGUI extends JFrame {
 
     @Serial
     private static final long serialVersionUID = 1L;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConcurrentGUI.class);
     private final JLabel display = new JLabel("0");
 
 
@@ -35,10 +41,77 @@ public final class ConcurrentGUI extends JFrame {
         panel.add(down);
         panel.add(stop);
 
+        /*
+         * Create the counter agent and start it. This is actually not so good:
+         * thread management should be left to
+         * java.util.concurrent.ExecutorService
+         */
+        final Agent agent = new Agent();
+        new Thread(agent).start();
+        /*
+         * Register a listener that stops it
+         */
+        stop.addActionListener(e -> { 
+            agent.stopCounting(); 
+            up.setEnabled(false); 
+            down.setEnabled(false);
+            stop.setEnabled(false); 
+        });
+        up.addActionListener(e -> agent.setUp());
+        down.addActionListener(e -> agent.setDown());
+
         //adding the panel with all the data and set visible the frame
         this.getContentPane().add(panel);
         this.setVisible(true);
 
         
     }
+
+    /*
+     * The counter agent is implemented as a nested class. This makes it
+     * invisible outside and encapsulated.
+     */
+    private final class Agent implements Runnable {
+        
+        private volatile boolean stop;
+        private volatile boolean up = true;        
+        private int counter;
+        
+
+        @Override
+        public void run() {
+            while (!this.stop) {
+                try {
+                    // The EDT doesn't access `counter` anymore, it doesn't need to be volatile
+                    final var nextText = Integer.toString(this.counter);
+                    SwingUtilities.invokeAndWait(() -> ConcurrentGUI.this.display.setText(nextText));
+                    if(this.up) {
+                        this.counter++;
+                    } else {
+                        this.counter--;
+                    }                    
+                    Thread.sleep(100);
+                } catch (InvocationTargetException | InterruptedException ex) {
+                    LOGGER.error(ex.getMessage(), ex);
+                }
+            }
+        }
+
+        /**
+         * External command to stop counting.
+         */
+        public void stopCounting() {
+            this.stop = true;
+        }
+
+        public void setUp() {
+            this.up = true;
+        }
+
+        public void setDown() {
+            this.up = false;
+        }
+    }
+
+
 }
